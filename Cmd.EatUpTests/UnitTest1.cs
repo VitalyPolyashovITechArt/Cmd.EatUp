1748b844c142c033b704fabdb34b4ed4ccedad59
Vyacheslav Bakhtin <slavabah@tut.by>
2014-06-14 14:24:58 +0300

    SmgAdapter added

---
+++ Cmd.EatUpTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cmd.EatUpTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var adapter = new SmgAdapter();
+            var sessionId = adapter.Authenticate("vyacheslav.bakhtin", "buypowerball");
+            var employees = adapter.GetAllEmployees(sessionId);
+            Debug.WriteLine("Employees count: ", employees.Count);
+        }
+    }
+}
