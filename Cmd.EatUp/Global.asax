d42dd9dd22f11f5084e26cc0b2cedc278f8350fe
Vitaly Polyashov <vitaly.polyashov@itechart-group.com>
2014-06-14 12:03:24 +0300

    initial

---
+++ Cmd.EatUp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cmd.EatUp.WebApiApplication" Language="C#" %>
---
+++ Cmd.EatUp/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Cmd.EatUp.Startup))]
+
+namespace Cmd.EatUp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
