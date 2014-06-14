eed6955e526e90da60790a98c6ea04a31dcb7143
Vitaly Polyashov <vitaly.polyashov@itechart-group.com>
2014-06-14 19:06:59 +0300

    employee controller is added

--- Cmd.EatUp/Controllers/EmployeesController.cs
+++ Cmd.EatUp/Controllers/EmployeesController.cs
@@ -1,17 +1,39 @@
-﻿using Cmd.EatUp.Data;
+﻿using System.Reflection.Emit;
+using System.Web.Http;
+using Cmd.EatUp.Data;
+using Cmd.EatUp.Models;
 using Cmd.EatUpTests;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
+using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.Owin.Security;
 
 namespace Cmd.EatUp.Controllers
 {
     public class EmployeesController : ApiController
     {
+    public class ResponseObject
+    {
+        public bool Success { get; set; }
+
+        public string FaultMessage { get; set; }
+    }
+
+    public class GetEmployeesResult : ResponseObject
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+    }
+
+    public class AuthenticateResult : ResponseObject
+    {
+        public string SessionId { get; set; }
+    }
         // GET api/<controller>
+       //[Route("")]
+        //[HttpGet]
         public void Fill()
         {
             var adapter = new SmgAdapter();
@@ -21,25 +43,35 @@ namespace Cmd.EatUp.Controllers
             repository.SaveEmployees(employees);
         }
 
-        // GET api/<controller>/5
-        public string Get(int id)
+        [System.Web.Http.Route("Authenticate")]
+        [System.Web.Http.HttpGet]
+        public string Authenticate(string userName, string password)
         {
-            return "value";
+            string sessionId = String.Empty;
+            var smgAdapter = new SmgAdapter();
+            try
+            {
+                sessionId = smgAdapter.Authenticate(userName, password);
+                return sessionId;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
 
-        // POST api/<controller>
-        public void Post([FromBody]string value)
+        [System.Web.Mvc.Route("GetProfileInfo/{id}")]
+
+        public ProfileInfoViewModel GetProfileInfo(int id)
         {
+            ProfileInfoViewModel viewModel = new ProfileInfoViewModel();
+           DatabaseRepository repository = new DatabaseRepository();
+            var result = repository.GetProfile(id);
+            viewModel.FirstName = result.FirstName;
+            return viewModel;
         }
 
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
+
---
+++ Cmd.EatUp/Models/ProfileInfoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Cmd.EatUp.Data;
+
+namespace Cmd.EatUp.Models
+{
+    public class ProfileInfoViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime StartPreferredTime { get; set; }
+        public DateTime FinishPreferredTime { get; set; }
+        public List<Achievement> Achievements { get; set; }
+
+        public List<Meeting> Invitations { get; set; }
+    }
+}
